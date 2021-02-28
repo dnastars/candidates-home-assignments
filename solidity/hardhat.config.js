7c5f577e3ba603a9e75134a0498b4809b725f6f5
Arnaud Desvachez <arnaud.desvachez@gmail.com>
2021-02-28 23:15:40 +0100

    Fix Metamask issue ChainId

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -4,8 +4,14 @@ require("@nomiclabs/hardhat-web3");
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
-// require("./tasks/faucet");
+require("./tasks/faucet");
 
 module.exports = {
-  solidity: "0.7.0"
+  solidity: "0.7.0",
+  networks: {
+  hardhat: {
+    chainId: 1337
+  }
+}
 };
+
